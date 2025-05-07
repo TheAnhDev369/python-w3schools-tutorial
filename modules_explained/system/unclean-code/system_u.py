99e577c500c89a99b05ffb972905e56e9cfcc931
TheAnhDev369 <theanhdev369@gmail.com>
2025-05-07 23:57:43 +0700

    chore: add clean-code and unclean-code folders with modules-explained\system

---
+++ modules_explained/system/unclean-code/system_u.py
@@ -0,0 +1,38 @@
+# sys là một module tích hợp (built-in module) quan trọng trong Python.
+# sys cung cấp các BIẾN và HÀM để tương tác với trình thông dịch Python và môi trường hệ thống.
+
+#   Import sys
+#   Module sys cung cấp các BIẾN và các HÀM để tương tác với tình thông dịch python và hệ thống.
+import sys
+
+#   print(sys.version) là in ra thông tin phiên bản của trình thông dịch Python (thuộc tính version của module sys )
+print(sys.version);
+print("\n");
+
+# print(sys.excutable) để lấy và in ra đường dẫn đến thư mục chứa file python (thuộc tính excutable của module sys)
+print(sys.executable);
+print("\n");
+
+# print (sys.path) để lấy và in đường dẫn đến thư mục chứa file python (thuộc tính path của module sys)
+print(sys.path);
+print("\n");
+
+# print(sys.platform) để lấy thông tin về hệ thống và in ra thông tin hệ thống (thuộc tính platform của module sys)
+print(sys.platform);
+print("\n");
+
+# print("\n") để in ra khoảng trống xuống dòng
+# Xuống dòng
+print("\n");
+
+# Chạy file python trong terminal bằng cách sử dụng python + tên file
+# Có thể code python trong terminal bằng cách sử dụng python -m + tên file
+
+# python -m system
+# python -m system là chạy file system.py trong terminal
+
+# Sử dụng exit() để thoát khỏi terminal
+
+# Sử dụng , gõ python vào terminal và nhấn enter để vào python shell
+# Python Shell là môi trường giao diện dòng lệnh (command line interface) cho phép người dùng nhập và thực thi mã Python trực tiếp
+
